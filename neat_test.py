e9b8f05897f7a4bf5590bc6e047e093a11d9420c
Tjerko Kieft <64585807+tjerkok@users.noreply.github.com>
2021-09-17 16:13:29 +0200

    added pickle for saving winner file

--- neat_test.py
+++ neat_test.py
@@ -1,7 +1,8 @@
 #from __future__ import print_function
 import sys, os
 sys.path.insert(0, 'evoman') 
-import neat           
+import neat       
+import pickle   
 import numpy as np
 from environment import Environment
 from player_controllers import player_controller
@@ -26,10 +27,10 @@ def fitness_player(genomes, config):
         # g.fitness = 0
         g.fitness = env.play(pcont=g)[0]
         f_g.append(g.fitness)
-    fitness_gens.append(np.mean(f_g)) # adding mean fitness to list
-    np.save('fitness_gens', fitness_gens) # saving to numpy file, opening in test.py
-    fitness_max.append(np.max(f_g))
-    np.save('fitness_max', fitness_max)
+    fitness_gens.append(np.mean(f_g))       # adding mean fitness to list
+    np.save('fitness_gens', fitness_gens)   # saving to numpy file, opening in test.py
+    fitness_max.append(np.max(f_g))         # adding max fitness to list
+    np.save('fitness_max', fitness_max)     # saving to numpy file, opening in test.py
         
 
 def run(config_file):
@@ -52,6 +53,11 @@ def run(config_file):
 
     # Display the winning genome.
     print('\nBest genome:\n{!s}'.format(winner))
+    # Stackoverflow on how to save the winning file and open it: https://stackoverflow.com/questions/61365668/applying-saved-neat-python-genome-to-test-environment-after-training
+    with open("winner.pkl", "wb") as f:
+        pickle.dump(winner, f)
+        f.close()
+
 
     
 if __name__ == '__main__':
@@ -60,6 +66,6 @@ if __name__ == '__main__':
     # current working directory.
     local_dir = os.path.dirname(__file__)
     config_path = os.path.join(local_dir, 'neat_config_file.txt')
-    fitness_gens = [] # list for mean fitness per generation
-    fitness_max = []
+    fitness_gens = []       # list for mean fitness per generation
+    fitness_max = []        # list for mean fitness per generation
     run(config_path)
