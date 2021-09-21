7edf6e79b3b75255c7434e2adabc2aea07ffc41a
Tjerko Kieft <64585807+tjerkok@users.noreply.github.com>
2021-09-21 11:51:38 +0200

    Update boxplot.py

--- boxplot.py
+++ boxplot.py
@@ -1,4 +1,5 @@
 
+from neat_test import N_runs
 import neat
 import pickle
 import numpy as np
@@ -6,8 +7,10 @@ import sys, os
 sys.path.insert(0, 'evoman') 
 from environment import Environment
 from player_controllers import player_controller
+from box_plot_test import boxplot
 
 experiment_name="neat"
+N_runs = 10
 n_hidden = 10
 local_dir = os.path.dirname(__file__)
 config_path = os.path.join (local_dir,'neat_config_file.txt')
@@ -17,7 +20,8 @@ env = Environment(experiment_name=experiment_name,
                   player_controller=player_controller(n_hidden))
 
 
-def replay_genome(config_path, genome_path=f"{experiment_name}/winner_2.pkl"):
+def replay_genome(config_path, run_i, experiment_name):
+    genome_path = f"{experiment_name}/winner_{i}.pkl"
     # Load requried NEAT config
     config = neat.config.Config(neat.DefaultGenome, neat.DefaultReproduction, neat.DefaultSpeciesSet, neat.DefaultStagnation, config_path)
 
@@ -32,11 +36,17 @@ def replay_genome(config_path, genome_path=f"{experiment_name}/winner_2.pkl"):
         fitness = env.play(pcont=g)[0]
     return fitness
 
-def five_runs():
+def five_runs(run_i, experiment_name):
     f_r = []
     for i in range(0,5):
-        fit = replay_genome(config_path)
+        fit = replay_genome(config_path, run_i, experiment_name)
         f_r.append(fit)
     return f_r
 
-print(five_runs())
+# print(five_runs())
+fitnesses = np.zeros((N_runs, 5))
+print(fitnesses.shape)
+for i in range(N_runs):
+    fitnesses[i,:] = five_runs(i, experiment_name)
+np.save(f"{experiment_name}/boxplotfitness", fitnesses)
+boxplot(fitnesses)
