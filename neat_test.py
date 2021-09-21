01050212f6b611e0f84ccfcb535e0806b3577848
Tjerko Kieft <64585807+tjerkok@users.noreply.github.com>
2021-09-21 14:08:33 +0200

    Fixed merge

--- neat_test.py
+++ neat_test.py
@@ -21,12 +21,8 @@ if not os.path.exists(experiment_name):
 
 env = Environment(experiment_name=experiment_name,
                   playermode="ai",
-<<<<<<< Updated upstream
-                  player_controller=player_controller())
-=======
-                  player_controller=player_controller(n_hidden),
+                  player_controller=player_controller(),
                   enemies=[3])
->>>>>>> Stashed changes
 
 
 def fitness_player(genomes, config):
