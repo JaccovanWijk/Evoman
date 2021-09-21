af7221d5712061adfdf074b5601c51f327ecd2ae
Lars1998 <90910928+Lars1998@users.noreply.github.com>
2021-09-21 13:18:47 +0200

    test test2

--- neat_test.py
+++ neat_test.py
@@ -22,7 +22,7 @@ if not os.path.exists(experiment_name):
 env = Environment(experiment_name=experiment_name,
                   playermode="ai",
                   player_controller=player_controller())
-#hoi
+
 
 def fitness_player(genomes, config):
     global i
