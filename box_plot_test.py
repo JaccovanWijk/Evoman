0be6db9426e9945373c1945ccf1edb72a3035df5
Tjerko Kieft <64585807+tjerkok@users.noreply.github.com>
2021-09-21 11:51:31 +0200

    making box plot for every run

--- box_plot_test.py
+++ box_plot_test.py
@@ -1,13 +1,15 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-x = np.arange(0,10)
-values = np.random.random((10,100))
-mean = np.mean(values, axis=1)
-stdev = np.std(values, axis=1)
+def boxplot(fitnesses):
+    plt.figure()
+    x = np.arange(fitnesses.shape[0])
+    for i in x:
+        values = fitnesses[i,:]
+        mean = np.mean(values, axis=0)
+        stdev = np.std(values, axis=0)
 
-data = np.concatenate((mean, mean+stdev, mean-stdev))
+        data = np.concatenate((mean, mean+stdev, mean-stdev))
 
-plt.figure()
-plt.boxplot(data)
-plt.show()
+        plt.boxplot(data)
+    plt.show()
