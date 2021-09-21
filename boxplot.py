0ac6a8475ab63bc956aadd5263b4e584402414ce
FloTig <floriantiggeloven@hotmail.com>
2021-09-21 10:11:40 +0200

    Boxplot bestand

---
+++ boxplot.py
@@ -0,0 +1,42 @@
+
+import neat
+import pickle
+import numpy as np
+import sys, os
+from environment import Environment
+sys.path.insert(0, 'evoman') 
+from player_controllers import player_controller
+
+experiment_name="neat"
+n_hidden = 10
+local_dir = os.path.dirname(__file__)
+config_path = os.path.join (local_dir,'neat_config_file.txt')
+
+env = Environment(experiment_name=experiment_name,
+                  playermode="ai",
+                  player_controller=player_controller(n_hidden))
+
+
+def replay_genome(config_path, genome_path=f"{experiment_name}/winner_0.pkl"):
+    # Load requried NEAT config
+    config = neat.config.Config(neat.DefaultGenome, neat.DefaultReproduction, neat.DefaultSpeciesSet, neat.DefaultStagnation, config_path)
+
+    # Unpickle saved winner
+    with open(genome_path, "rb") as f:
+        genome = pickle.load(f)
+
+    # Convert loaded genome into required data structure
+    genomes = [(1, genome)]
+
+    for genome_id, g in genomes:
+        fitness = env.play(pcont=g)[0]
+    return fitness
+
+def five_runs():
+    f_r = []
+    for i in range(0,5):
+        fit = replay_genome(config_path)
+        f_r.append(fit)
+    return f_r
+
+print(five_runs())
