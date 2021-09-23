0666a4efc99189cd5fee0ec11146a6b94a1c91f8
Jacco <jaccov.wijk@yahoo.com>
2021-09-23 12:53:41 +0200

    Update test_algorithm.py

--- test_algorithm.py
+++ test_algorithm.py
@@ -7,7 +7,7 @@ import neat
 import pickle   
 import numpy as np
 from evoman.environment import Environment
-from player_controllers import player_controller
+from player_controllers import player_controller2
 from plot import plot_fitness
 
 # choose this for not using visuals and thus making experiments faster
@@ -20,20 +20,41 @@ if not os.path.exists(experiment_name):
     os.makedirs(experiment_name)
    
 enemy = 1
+n_hidden = 10
 env = Environment(experiment_name=experiment_name,
                   playermode="ai",
-                  player_controller=player_controller(),
+                  player_controller=player_controller2(n_hidden),
                   enemies=[enemy],
                   randomini="yes")
 
 def fitness(population):
-    # TODO: Check fitness
-    return [x is 0 for x in range(len(population))]
+    pop_fitness = []
+    for individual in population:
+        reshaped = np.reshape(individual, (1,len(individual)))
+        fitness = env.play(pcont=reshaped)[0]
+        pop_fitness.append(fitness)
+    
+    fitness_gens.append(np.mean(pop_fitness))       # adding mean fitness to list
+    np.save(f"{experiment_name}/fitness_gens_{i}", fitness_gens)   # saving to numpy file, opening in test.py
+    fitness_max.append(np.max(pop_fitness))         # adding max fitness to list
+    np.save(f"{experiment_name}/fitness_max_{i}", fitness_max)     # saving to numpy file, opening in test.py
+     
+    return pop_fitness
 
 def offspring(solutions):
     # TODO: Create offsprings according to the algorithm
     # Find fitness only for the new population
-    return solutions
+    population, pop_fitness = solutions
+    new_population = [[]]
+    chances = (pop_fitness+10)/110 #normalize -10/100 to 0/1
+    while len(new_population) < len(population):
+        parents = np.random.choice(population, 2, p=chances) # Pick two parents based on their fitness
+        child = parents[1][:len(parents[1])] + parents[2][len(parents[2]):]
+        new_population[0].add(child)
+    return new_population
+
+fitness_gens = []
+fitness_max = []
 
 pop_size = 30
 gen = 20
@@ -55,8 +76,10 @@ solutions = [pop, pop_fitness]
 env.update_solutions(solutions)
 
 for i in range(gen):
-    solutions = offspring(solutions)
-    pop, pop_fitness = solutions
+    pop = offspring(solutions)
+    pop_fitness = fitness(pop)
+    solutions = [pop, pop_fitness]
+    env.update_solutions(solutions)
     
     new_best = np.argmax(pop_fitness)
     if new_best > best_each_gen[-1]:
@@ -67,4 +90,5 @@ for i in range(gen):
     
     mean_each_gen.append(np.mean(pop_fitness))
     std_each_gen.append(np.std(pop_fitness))
-    
+
+# TODO: Save winner
